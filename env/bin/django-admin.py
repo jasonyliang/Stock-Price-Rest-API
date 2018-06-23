c04b06bf3007b8004b5ae267e20b3ce1a919cfb9
Jason Liang <jjjasonliang@gmail.com>
2018-06-23 11:35:33 +0900

    set up API

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jhajhajhajha1/Desktop/Github/Stock-Price-Rest-API/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
